b5c7346b91010f0c15f6ab94fe9fe9df145d8e45
JaeMin Cheon <elrion018@gmail.com>
2019-04-13 14:57:09 +0900

    PS

--- beakjoon_PS/no1912.py
+++ beakjoon_PS/no1912.py
@@ -1,14 +1,15 @@
-import sys
-n = list(map(int, sys.stdin.readline().split()))
-nums = list(map(int,sys.stdin.readline().split()))
-numsSum = 0
-sumList = []
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        numsSum = 0
-        for k in range(i, j+1):
-            numsSum += nums[k]
-        sumList.append(numsSum)
+#############
+#백준 1912번 문제 연속합
+#D[i] : i번째 항을 마지막으로하는 수열의 합(연속합 )의 최댓값
+#D[i] = max(0, D[i-1]) + A[i] 0을 기준으로 분기를 나눈다.
+##########################
 
-sumList = list(set(sumList))
-print(max(sumList))
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int,sys.stdin.readline().split()))
+D = []
+D.append(A[0])
+for i in range(1,N):
+    D.append(0)
+    D[i] = max(0,D[i-1]) + A[i]
+print(max(D))
