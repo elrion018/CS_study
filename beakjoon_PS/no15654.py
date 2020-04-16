8680c4b3effbac8196430639926bef9194ab79be
Daniel <elrion018@gmail.com>
2020-04-16 18:55:28 +0900

    20200416

---
+++ beakjoon_PS/no15654.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def backTracking(N, M, count, arr, checked, result):
+    if count == M:
+        return print(" ".join(map(str, result)))
+
+    for i in range(N):
+        if checked[i] == False:
+            result.append(arr[i])
+            checked[i] = True
+            backTracking(N, M, count+1, arr, checked, result)
+            result.pop()
+            checked[i] = False
+
+
+N, M = map(int, sys.stdin.readline().split())
+arr = sorted(list(map(int, sys.stdin.readline().split())))
+count = 0
+checked = [False for _ in range(N)]
+result = []
+backTracking(N, M, count, arr, checked, result)
