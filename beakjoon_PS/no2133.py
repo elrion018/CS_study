a35dee6f1431cb360d8cd5b8bc52362b52665b36
Daniel <elrion018@gmail.com>
2020-09-01 17:21:50 +0900

    백준 2133

---
+++ beakjoon_PS/no2133.py
@@ -0,0 +1,12 @@
+# D[i] = i번째 항을 마지막으로 하는 가장 긴 증가하는 부분 수열의 길이
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+D = [1 for i in range(N)]
+for i in range(1, N):
+    D[i] = 1
+    for j in range(i, -1, -1):
+        if A[j] < A[i] and D[j] >= D[i]:
+            D[i] = D[j] + 1
+
+print(max(D))
