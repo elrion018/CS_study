189231245efa162e4006091f67217853cb4b7f05
Daniel <elrion018@gmail.com>
2020-09-26 20:30:41 +0900

    백준 2164번

---
+++ beakjoon_PS/no2164_2.py
@@ -0,0 +1,13 @@
+import sys
+import collections
+
+N = int(sys.stdin.readline())
+queue = collections.deque()
+
+for i in range(1, N+1):
+    queue.append(i)
+
+while len(queue) > 1:
+    queue.popleft()
+    queue.append(queue.popleft())
+print(queue[0])
