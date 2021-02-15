5c4072909475556d3323fbda77a48a17101e57ac
Daniel <elrion018@gmail.com>
2021-02-15 17:51:14 +0900

    백준 1927번

---
+++ beakjoon_PS/no1927.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+
+
+def insert(x):
+    heapq.heappush(q, x)
+    return
+
+
+def pop():
+    if len(q) == 0:
+        return 0
+    else:
+        return heapq.heappop(q)
+
+
+q = []
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        print(pop())
+    else:
+        insert(x)
