38a8e9275879d092c3d8596b26c5f78d8243a138
Daniel <elrion018@gmail.com>
2021-03-09 19:18:38 +0900

    백준 1958번

---
+++ beakjoon_PS/no1958.py
@@ -0,0 +1,16 @@
+import sys
+
+a = list(sys.stdin.readline().strip())
+b = list(sys.stdin.readline().strip())
+c = list(sys.stdin.readline().strip())
+
+dp = [[[0]*(len(c)+1) for _ in range(len(b) + 1)] for _ in range(len(a)+1)]
+
+for i in range(1,len(a)+1):
+	for j in range(1, len(b)+1):
+		for k in range(1, len(c)+1):
+			if a[i-1] == b[j-1] == c[k-1]:
+				dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+			else:
+				dp[i][j][k] = max(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+print(dp[len(a)][len(b)][len(c)])
